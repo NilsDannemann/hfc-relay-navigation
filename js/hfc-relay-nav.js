17ed2ade977a3e2953dffd3fd2a85ed68ac1f77f
Lars Mehrhoff <lm@hfci.de>
2017-01-24 11:28:04 +0100

    [fix] Dropdowns

--- js/hfc-relay-nav.js
+++ js/hfc-relay-nav.js
@@ -8,17 +8,17 @@
     if (el.classList) {
       el.classList.toggle(className);
     } else {
-          var classes = el.className.split(' ');
-          var existingIndex = classes.indexOf(className);
+      var classes = el.className.split(' ');
+      var existingIndex = classes.indexOf(className);
 
-          if (existingIndex >= 0) {
-              classes.splice(existingIndex, 1);
-          } else {
-              classes.push(className);
-          }
-
-          el.className = classes.join(' ');
+      if (existingIndex >= 0) {
+        classes.splice(existingIndex, 1);
+      } else {
+        classes.push(className);
       }
+
+      el.className = classes.join(' ');
+    }
   };
 
   /**
@@ -28,23 +28,23 @@
    * @return {Boolean|Element} Returns false if not match found
    */
   var getClosest = function (elem, selector) {
-      var firstChar = selector.charAt(0);
-      for (; elem && elem !== document; elem = elem.parentNode) {
-          if (firstChar === '.') {
-              if (elem.classList.contains(selector.substr(1))) {
-                  return elem;
-              }
-          } else if (firstChar === '#') {
-              if (elem.id === selector.substr(1)) {
-                  return elem;
-              }
-          } else if (firstChar === '[') {
-              if (elem.hasAttribute(selector.substr(1, selector.length - 2))) {
-                  return elem;
-              }
-          }
+    var firstChar = selector.charAt(0);
+    for (; elem && elem !== document; elem = elem.parentNode) {
+      if (firstChar === '.') {
+        if (elem.classList.contains(selector.substr(1))) {
+          return elem;
+        }
+      } else if (firstChar === '#') {
+        if (elem.id === selector.substr(1)) {
+          return elem;
+        }
+      } else if (firstChar === '[') {
+        if (elem.hasAttribute(selector.substr(1, selector.length - 2))) {
+          return elem;
+        }
       }
-      return false;
+    }
+    return false;
   };
 
   /*
@@ -52,21 +52,23 @@
    */
   var submenu = document.querySelectorAll('.has-submenu');
   Array.from(submenu).forEach(item => {
-      item.addEventListener('click', function(event) {
-          toggleClass(this, 'is-open');
-          toggleClass(this.querySelector('ul'), 'show');
-      });
+    item.getElementsByTagName('a')[0].addEventListener('click', function(event) {
+      var parent = event.target.parentNode;
+
+      toggleClass(parent, 'is-open');
+      toggleClass(parent.querySelector('ul'), 'show');
+    });
   });
 
   /*
    * Remove when clicked outside dropdown
    */
   document.addEventListener('click', function (event) {
-      if (!getClosest(event.target, '.has-submenu') && event.target !== this.querySelector('.has-submenu')) {
-        Array.from(submenu).forEach(item => {
-            item.classList.remove('is-open');
-            item.querySelector('ul').classList.remove('show');
-        });
-      }
+    if (!getClosest(event.target, '.has-submenu') && event.target !== this.querySelector('.has-submenu')) {
+      Array.from(submenu).forEach(item => {
+        item.classList.remove('is-open');
+        item.querySelector('ul').classList.remove('show');
+      });
+    }
   });
 })();
